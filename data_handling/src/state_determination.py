ec901b21158b302bbef8946a3a48eda2effa55de
zlaouar8185 <zala9003@colorado.edu>
2019-10-31 16:37:53 -0600

    replaced with correct file

--- data_handling/src/state_determination.py
+++ data_handling/src/state_determination.py
@@ -4,43 +4,42 @@ import sys
 import os
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from depth-listener.msg import *
 from std_msgs.msg import Bool
 
-def imageDepthCallback(self, data):
-    try:
-        cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
-        pix = (data.width/2, data.height/2)
-        sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
-        self.center = cv_image[pix[1], pix[0]]
-        self.right = cv_image[data.width, pix[1]]
-        self.left = cv_image[0, pix[1]]
-        sys.stdout.flush()
-    except CvBridgeError as e:
-        print(e)
-        return
-    
-def listener():
+class ImageListener:
 	def __init__(self):
-    	rospy.init_node('state', anonymous=True)
+		self.image_topic = "/camera/depth/image_rect_raw"
+		self.bridge = CvBridge()
 
-    	rospy.Subscriber("/camera/depth/image_rect_raw", Image, callback)
-    	state_pub = rospy.Publisher("/state", Bool, queue_size=1)
+		rospy.init_node('state_node', anonymous=True)
 
-    	# spin() simply keeps python from exiting until this node is stopped
+		self.image_sub = rospy.Subscriber(self.image_topic, Image, self.imageDepthCallback)
+		self.state_pub = rospy.Publisher("/state", Bool, queue_size=1)
+		
+	def imageDepthCallback(self, data):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
+			pix = (data.width/2, data.height/2)
+			sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.image_topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
+			self.center = cv_image[pix[1], pix[0]]
+			self.right = cv_image[data.width, pix[1]]
+			self.left = cv_image[0, pix[1]]
+			sys.stdout.flush()
+		except CvBridgeError as e:
+			print(e)
+			return
 
-    	print self.center
-    	#Assume: velocity is constant and slow
-    	#isolate middle 5 values, 45 deg left, 45 deg right
-    	#average each
+		print(self.center)
+		#Assume: velocity is constant and slow
+		#isolate middle 5 values, 45 deg left, 45 deg right
+		#average each
 
-    	while not rospy.is_shutdown(): #true = 1 = hallway, false = 2 = turn
-    		if self.center/1000 > 2 and (self.right - self.left) < 20:
-	    		state_pub.publish(True)
-	    	else:
-	    		state_pub.publish(False)
-
-    	rospy.spin()
+		#true = 1 = hallway, false = 2 = turn
+		if self.center/1000 > 2 and (self.right - self.left) < 20:
+			self.state_pub.publish(True)
+		else:
+			self.state_pub.publish(False)
 
 if __name__ == '__main__':
-    listener()
+    ImageListener()
+    rospy.spin()
